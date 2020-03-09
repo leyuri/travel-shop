37dab4cd733821d45021c381cb6d69d13e177d46
leyuri <leyuri97@gmail.com>
2020-03-09 21:18:59 +0900

    Add LandingPage Template

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { FaCode } from "react-icons/fa";
 import Axios from 'axios';
-import { response } from 'express';
+import { Icon, Col, Card, Row } from 'antd';
+
+
+const { Meta } = Card;
 
 function LandingPage() {
 
@@ -27,17 +29,58 @@ function LandingPage() {
 
     }, [])
 
+    const renderCards = Products.map((product, index) => {
+
+        //하나의 행은 24사이즈, 라지사이즈일 때는 6, 미디움은 8
+        return <Col lg={6} md={8} xs={24}>
+            <Card
+                hoverable={true}
+                cover
+            >
+                <Meta
+                    title={product.title}
+                    description={`$${product.price}`}
+                />
+            </Card>
+        </Col>
+    })
+
 
 
     return (
-        <>
+        <div style={{ width: '75%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h2>  Let's Travel Anywhere  <Icon type="rocket" />  </h2>
+            </div>
 
 
+            {/* Filter  */}
+
+            <Row gutter={[16, 16]}>
+                {Products.map((product, index)=> {})}
+            {renderCards}
+            </Row>
+
+            {/* Search  */}
 
 
+            {Products.length === 0 ?
+                <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+                    <h2>No post yet...</h2>
+                </div> :
+                <div>
 
 
-        </>
+                </div>
+            }
+            <br /><br />
+
+
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button onClick >Load More</button>
+                </div>
+    
+        </div>
     )
 }
 
