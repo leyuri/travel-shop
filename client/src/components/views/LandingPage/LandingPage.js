e1b6173c5c23385c02955de8f1adbc7ae0e77cac
leyuri <leyuri97@gmail.com>
2020-03-09 22:46:37 +0900

    Add ImageSlider

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios';
 import { Icon, Col, Card, Row } from 'antd';
+import ImageSlider from '../../utils/ImageSlider';
 
 
 const { Meta } = Card;
@@ -35,7 +36,7 @@ function LandingPage() {
         return <Col lg={6} md={8} xs={24}>
             <Card
                 hoverable={true}
-                cover
+                cover={<ImageSlider images={product.images}/>}
             >
                 <Meta
                     title={product.title}
