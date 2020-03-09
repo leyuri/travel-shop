27ecaed47aa01c5c2f6fce2d82cf68964e850d23
leyuri <leyuri97@gmail.com>
2020-03-10 01:55:42 +0900

    Add onChange Func for Checkbox

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Checkbox, Collapse } from 'antd';
 
 const { Panel } = Collapse
@@ -36,11 +36,41 @@ const continents = [
 
 function CheckBox() {
 
+    const [Checked, setChecked] = useState([])
+
+    const handleToggle = (value) => {
+
+        Checked: [1,2,3]
+        Checked: [1,2,3,4]
+
+        const currentIndex = Checked.indexOf(value);
+        //output = -1 이 나왔다는 것은 checked 되지 않음을 의미함
+        //아직 없을 경우에
+        // indexOf -1 , 0, 1, 2, 3
+        const newChecked = [...Checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value)
+        } else {
+            newChecked.splice(currentIndex, 1)
+            //체크가 되어있는 상황이라면?
+            //splice 는 배열에서 특정 범위의 값들을 추출하고, 그 자리에 새로운 값을 넣는다
+            //array.splice(index, howmany, element1, ...., elementN);
+            //index:배열에 추가할 특정 배열의 위치를 가르키는 index, howmany:
+            // index에서부터 제거될 원소들의 수. index부터 index+howmany에 해당하는 원소들은 삭제된다. 이 값이 0이면 어떠한 원소도 삭제되지 않는다.
+        }
+
+        setChecked(newChecked)
+        props.handleFilters(newChecked)
+        //update this checked information into Parent Component 
+
+    }
+
 
     const renderCheckboxLists = () => continents.map((value, index) => (
         <React.Fragment key={index}>
             <Checkbox
-                onChange
+                onChange={()=>handleToggle(value._id)}
                 type="checkbox"
                 checked
             />&nbsp;&nbsp;
