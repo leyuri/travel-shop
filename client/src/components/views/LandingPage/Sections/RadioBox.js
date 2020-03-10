fe6486817b217fa373b767a4337d9b3ef21ade95
leyuri <leyuri97@gmail.com>
2020-03-11 02:32:02 +0900

    Add onChange Func for Radiobox

--- client/src/components/views/LandingPage/Sections/RadioBox.js
+++ client/src/components/views/LandingPage/Sections/RadioBox.js
@@ -5,18 +5,25 @@ const { Panel } = Collapse;
 
 function RadioBox(props) {
 
+    const [Value, setValue] = useState('0')
+    //가격 시작은 0부터
+
     const renderRadioBox = () => (
         props.list &&  props.list.map((value) => (
             <Radio key={value._id} value={`${value._id}`}>{value.name}</Radio>
         ))
     )
 
+    const handleChange = (e) => {
+        setValue(e.target.value)
+        props.handleFilters(e.target.value)
+    }
 
     return (
         <div>
             <Collapse defaultActiveKey={['0']}>
                 <Panel header="price" key="1">
-                    <Radio.Group onChange value>
+                    <Radio.Group onChange={handleChange} value>
 
                         {renderRadioBox()}
 
