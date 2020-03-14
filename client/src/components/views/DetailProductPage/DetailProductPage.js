4e31aea88350e0bbbc881355171d51d2e546cb1c
leyuri <leyuri97@gmail.com>
2020-03-15 01:33:09 +0900

    Add Template for DetailProductPage

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import Axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { Row, Col } from 'antd';
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
 function DetailProductPage(props) {
 
 
@@ -18,8 +21,22 @@ function DetailProductPage(props) {
     }, [])
 
     return (
-        <div>
-            DetailProductPage
+        <div className="postPage" style={{ width: '100%', padding: '3rem 4rem' }}>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h1>Product title</h1>
+            </div>
+
+            <br />
+
+            <Row gutter={[16, 16]} >
+                <Col lg={12} xs={24}>
+                    <ProductImage />
+                </Col>
+                <Col lg={12} xs={24}>
+                    <ProductInfo />
+                </Col>
+            </Row>
         </div>
     )
 }
