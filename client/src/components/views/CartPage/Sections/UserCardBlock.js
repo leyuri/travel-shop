2c5c043f62313caf384b7ce1837af3414d6ff742
leyuri <leyuri97@gmail.com>
2020-03-17 19:36:21 +0900

    Change UserCardBlock

--- client/src/components/views/CartPage/Sections/UserCardBlock.js
+++ client/src/components/views/CartPage/Sections/UserCardBlock.js
@@ -3,7 +3,19 @@ import React from 'react'
 const UserCardBlock = () => {
     return (
         <div>
-            UserCardBlock
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product Image</th>
+                        <th>Product Quantity</th>
+                        <th>Product Price</th>
+                        <th>Remove from Cart</th>
+                    </tr>
+                </thead>
+                <tbody>
+      
+                </tbody>
+            </table>
         </div>
     )
 }
