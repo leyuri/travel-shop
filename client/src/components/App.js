6205f021aca25ea413d8c349e71bd98b46560692
leyuri <leyuri97@gmail.com>
2020-03-25 00:07:08 +0900

    Add a route for HistoryPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ import Footer from "./views/Footer/Footer";
 import UploadProductPage from "./views/UploadProductPage/UploadProductPage";
 import DetailProductPage from "./views/DetailProductPage/DetailProductPage";
 import CartPage from "./views/CartPage/CartPage";
-
+import HistoryPage from "./views/HistoryPage/HistoryPage"
 
 
 function App() {
@@ -27,6 +27,8 @@ function App() {
           <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
           {/* 회원만 디테일 페이지 접속 가능 */}
           <Route exact path="/user/cart" component={Auth(CartPage, true)} />
+          <Route exact path="/history" component={Auth(HistoryPage, true)} />
+          {/* 로그인 된 회원만 히스토리 화면 접속 가능 */}
         </Switch>
       </div>
       <Footer />
